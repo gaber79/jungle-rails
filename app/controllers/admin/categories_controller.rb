0c39e3776153511f896d94b3838622079c37b769
gaber79 <poetdreams@hotmail.com>
2016-11-13 06:34:49 +0000

    added categories and admin menu addition

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,26 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    @category.save
+    redirect_to @category
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name, :created_at)
+    end
+
+end
